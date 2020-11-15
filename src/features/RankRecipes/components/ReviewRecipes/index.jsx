bdff7efed040a7d6c496de4cc89baa1e96acd9ae
The_Lost_Owl <60459107+vungoctuyen1812@users.noreply.github.com>
2020-11-15 23:33:27 +0900

    update review component

--- src/features/RankRecipes/components/ReviewRecipes/index.jsx
+++ src/features/RankRecipes/components/ReviewRecipes/index.jsx
@@ -1,12 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-
+import './styles.scss';
+import ArrowForwardRoundedIcon from '@material-ui/icons/ArrowForwardRounded';
 ReviewRecipes.propTypes = {
-    reviews: PropTypes.array,
+    reviewsList: PropTypes.array,
+    handleNextReviewClick: PropTypes.func,
 };
 
-function ReviewRecipes({ reviews }) {
-    return <div className="rankRecipes__item"></div>;
+ReviewRecipes.defaultProps = { reviewsList: [], handleNextReviewClick: null };
+//get random review func
+function randomReview() {
+    const randomIndex = Math.trunc(Math.random() * 5);
+    return randomIndex;
+}
+//Component
+function ReviewRecipes({ reviewsList }) {
+    const [content, setContent] = useState(
+        {
+            id: 1,
+            date: 'November 14, 2020',
+            name: 'Miss Viet Nam  Review',
+            writer: 'By Maria Williams',
+            desc:
+                'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum eos ipsa animi iste, nobis possimus?',
+            comments: 2,
+            url:
+                'https://images.unsplash.com/photo-1543007631-283050bb3e8c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1934&q=80',
+        },
+        []
+    );
+
+    function handleNextReviewClick() {
+        const newReviewsList = reviewsList;
+        const index = randomReview();
+        const newRecipe = newReviewsList[index];
+        setContent(newRecipe);
+    }
+    return (
+        <div className="rankRecipes__item review__container">
+            <img className="review__img" src={content.url} alt={content.name} />
+            <p className="review__number">{content.id}</p>
+            <p className="review__date">{content.date}</p>
+            <div className="review__content">
+                <blockquote className="review__blockquote">{content.desc}</blockquote>
+                <cite className="review__cite">{content.writer}</cite>
+                <div className="review__btn--wrapper">
+                    <button className="review__btn" onClick={() => handleNextReviewClick()}>
+                        <ArrowForwardRoundedIcon />
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default ReviewRecipes;
--- src/features/RankRecipes/index.jsx
+++ src/features/RankRecipes/index.jsx
@@ -80,24 +80,68 @@ function RankRecipes(props) {
                 'https://images.unsplash.com/photo-1579065472192-c3f98f86b6c6?ixlib=rb-1.2.1&auto=format&fit=crop&w=2134&q=80',
         },
     ];
-    const reviews = [
+    const reviewsList = [
+        {
+            id: 0,
+            date: 'March 07, 2020',
+            name: 'Miss Viet Nam  Review',
+            writer: 'By Maria Williams',
+            desc:
+                'This cozy restaurant has left the best impressions! Hospitable hosts, delicious dishes, beautiful presentation, wide wine list and wonderful dessert. I recommend to everyone! I would like to come back here again and again.',
+            comments: 2,
+            url:
+                'https://images.unsplash.com/photo-1554118811-1e0d58224f24?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1930&q=80',
+        },
         {
             id: 1,
             date: 'November 14, 2020',
             name: 'Miss Viet Nam  Review',
-            writer: 'By Maria Williams',
+            writer: 'By Big Daddy',
             desc:
-                'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum eos ipsa animi iste, nobis possimus?',
+                'It’s a great experience. The ambiance is very welcoming and charming. Amazing wines, food and service. Staff are extremely knowledgeable and make great recommendations.',
             comments: 2,
             url:
-                'https://images.unsplash.com/photo-1543007631-283050bb3e8c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1934&q=80',
+                'https://images.unsplash.com/photo-1428515613728-6b4607e44363?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+        },
+        {
+            id: 2,
+            date: 'November 27, 2020',
+            name: 'Miss Viet Nam  Review',
+            writer: 'By Hatake Hana',
+            desc:
+                'A small local restaurant with great service, food, and overall experience! Definitely a go-to place for (FN) when you are in (CN)! Huge variety of (FN) to choose from & side dishes are delicious as well.',
+            comments: 1,
+            url:
+                'https://images.unsplash.com/photo-1579869696034-ec145eb3987c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+        },
+        {
+            id: 3,
+            date: 'December 22, 2020',
+            name: 'Miss Viet Nam  Review',
+            writer: 'By John Cena',
+            desc:
+                'Loved this place. The atmosphere, food and most of the drinks were great. The bartenders knew their mixology and it showed in the drinks. Great place to hangout, chill or go out on a date.',
+            comments: 3,
+            url:
+                'https://images.unsplash.com/photo-1571705042748-55feda1cfadc?ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80',
+        },
+        {
+            id: 4,
+            date: 'July 02, 2020',
+            name: 'Miss Viet Nam  Review',
+            writer: 'By Den Vau',
+            desc:
+                'Absolutely amazing place to eat, we will be making a reservation for our next visit in (CN)! 10 of 10 for every single aspect of this meal, and this is coming from people who know and love great food!',
+            comments: 4,
+            url:
+                'https://images.unsplash.com/photo-1488992783499-418eb1f62d08?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2053&q=80',
         },
     ];
     return (
         <div className="rankRecipes__container">
             <MostLikedRecipes mostLiked={mostLiked} />
             <TopRatedRecipes topRated={topRated} />
-            <ReviewRecipes reviews={reviews} />
+            <ReviewRecipes reviewsList={reviewsList} />
         </div>
     );
 }
